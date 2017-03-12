3d50cfcf6cb0006c623556ca6852480b96e5783b
Emanuel Amit <emanuel.amit@gmail.com>
2017-03-13 00:05:49 +0200

    Fixed Bluetooth library

---
+++ Meshendo/app/src/main/java/com/manustudios/meshendo/BluetoothManager.java
@@ -0,0 +1,17 @@
+package com.manustudios.meshendo;
+
+import android.app.Activity;
+
+/**
+ * Created by Emanuel on 3/13/2017.
+ */
+
+public class BluetoothManager extends BluetoothManagerUnmodified{
+
+    public BluetoothManager(Activity activity){
+        super(activity);
+    }
+
+
+
+}
--- Meshendo/app/src/main/java/com/manustudios/meshendo/MainActivity.java
+++ Meshendo/app/src/main/java/com/manustudios/meshendo/MainActivity.java
@@ -1,76 +1,27 @@
 package com.manustudios.meshendo;
 
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-import com.ramimartin.multibluetooth.activity.BluetoothActivity;
-import com.ramimartin.multibluetooth.bluetooth.mananger.BluetoothManager;
 
 
-public class MainActivity extends BluetoothActivity{
+public class MainActivity extends AppCompatActivity {
 
-    private Button serverBtn;
-    private Button clientBtn;
-    private Button clientConnectBtn;
+    private BluetoothManager bluetoothManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        serverBtn = (Button) findViewById(R.id.serverBtn);
-        serverBtn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Log.d("Emanuel", "Server started");
-//                Log.d("Emanuel", BluetoothAdapter.getDefaultAdapter().getAddress());
-                setTimeDiscoverable(BluetoothManager.BLUETOOTH_TIME_DICOVERY_3600_SEC);
-                selectServerMode();
-                scanAllBluetoothDevice();
-//                createClient(mServerAddressMac);
-            }
-        });
+        bluetoothManager = new BluetoothManager(this);
+
+        bluetoothManager.selectServerMode();
+
+
+
+
+
     }
 
-    @Override
-    public int myNbrClientMax() {
-        return 7;
-    }
-
-    @Override
-    public void onBluetoothStartDiscovery() {
-    }
-
-    @Override
-    public void onBluetoothDeviceFound(BluetoothDevice device) {
-    }
-
-    @Override
-    public void onClientConnectionSuccess() {
-    }
-
-    @Override
-    public void onClientConnectionFail() {
-    }
-
-    @Override
-    public void onServeurConnectionSuccess() {
-    }
-
-    @Override
-    public void onServeurConnectionFail() {
-    }
-
-    @Override
-    public void onBluetoothCommunicator(String messageReceive) {
-    }
-
-    @Override
-    public void onBluetoothNotAviable() {
-    }
 
 }
-
